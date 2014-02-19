60e8e00bec1aba15dd98d76cd9edf19756074fff
Batkhuu <bybatkhuu@yahoo.com>
2014-02-19 09:29:18 +0800

    Database name changed!

--- Online-Pharmacy-POS-project/Pharmacy-POS/src/utils/PostgreSQLJDBC.java
+++ Online-Pharmacy-POS-project/Pharmacy-POS/src/utils/PostgreSQLJDBC.java
@@ -27,7 +27,7 @@ public class PostgreSQLJDBC
     {
         server = "localhost";
         port = "5432";
-        database = "OnlinePOS";
+        database = "PharmacyPOS";
         user = "postgres";
         password = "root";
     }
